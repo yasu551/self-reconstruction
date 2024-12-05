83463ef50b3326e698857ef6e95f0196830160c7
Yasuhiro Sugawara <yasu@sonicgarden.jp>
2024-12-06 08:32:57 +0900

    importmapでdaisyuiを使おうとしたけどやめた

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -4,14 +4,27 @@ class GoalsController < ApplicationController
   end
 
   def new
+    @goal = current_user.goals.build
   end
 
   def create
+    @goal = current_user.goals.build(goal_params)
+    if @goal.save
+      redirect_to goals_path, notice: t('controllers.created')
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def goal_params
+    params.expect(goal: %i[content memo enabled])
   end
 end
